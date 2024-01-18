25af3e4a6e23aaf799f26ca5c05f7ac476ea9961
Manuel Parra-Royón <manuparra@gmail.com>
2024-01-18 15:41:51 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
+echo "Adding processMeekKAT to the path. Now you can use processMeerKAT from your data/code folder"
+echo "-----------------------"
 dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/processMeerKAT"
 export PATH=$PATH:$dir
 export PYTHONPATH=$PYTHONPATH:$dir
